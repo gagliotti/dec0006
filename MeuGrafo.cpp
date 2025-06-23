e4f7e48603c8a3227b3d98431bd84589fac49b1d
Martín Vigil <gagliotti@gmail.com>
2025-06-23 19:51:16 +0000

    codigo minimo para compilar

--- MeuGrafo.cpp
+++ MeuGrafo.cpp
@@ -8,9 +8,10 @@
 // std::size
 #include <queue>
 // std::priority_queue
+#include <functional>
+#include <utility>
 #include <vector>
 // std::vector
-
 #include "excecoes.h"
 // ExcecaoMatrizAdjacenciaVazia
 // ExcecaoMatrizAdjacenciaNaoQuadrada
@@ -20,3 +21,60 @@
 
 
 //implente aqui
+MeuGrafo::MeuGrafo(MatrizAdjacencia const& matrizAdjacencia):GrafoAbstrato(matrizAdjacencia){
+    // escreva o algoritmo esperado
+}
+
+std::size_t MeuGrafo::quantidadeVertices()const{
+    // substitua a linha abaixo pelo algoritmo esperado
+    return 0;
+};
+
+
+std::size_t MeuGrafo::quantidadeArestas(std::size_t vertice)const{
+    // substitua a linha abaixo pelo algoritmo esperado
+    return 0;
+};
+
+
+std::size_t MeuGrafo::quantidadeArestas() const{
+    // substitua a linha abaixo pelo algoritmo esperado
+    return 0;
+};
+
+double MeuGrafo::custo(std::size_t origem, std::size_t destino)const{
+    // substitua a linha abaixo pelo algoritmo esperado
+    return 0;
+};
+
+
+std::vector<double> MeuGrafo::menoresDistancias(std::size_t origem)const{
+    // substitua a linha abaixo pelo algoritmo esperado
+    return {0.0};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
